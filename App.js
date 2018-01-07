7f00cb92ca514cc756cf5b258e50aff2f9ba1454
yoojat <yooja.ty@gmail.com>
2018-01-07 13:07:10 +0900

    Creating Button Component

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Timer from './components/Timer'
+import {StyleSheet, Text, View} from 'react-native';
+import Timer from './components/Timer';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <Timer/>
-
-    );
+    return <Timer />;
   }
 }
 
